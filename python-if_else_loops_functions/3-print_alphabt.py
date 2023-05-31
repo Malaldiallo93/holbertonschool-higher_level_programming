52c3dddbad80e38a9d08941cb33971124215d7f3
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-05-31 02:24:52 +0200

    update a program print all the letters except q and e

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z') + 1):
-    if i != ord('e') and i != ord('q'):
-        print(chr(i), end='')
+    if i == ord('e') or i == ord('q'):
+        continue
+    print("{}".format(chr(i)), end="")
