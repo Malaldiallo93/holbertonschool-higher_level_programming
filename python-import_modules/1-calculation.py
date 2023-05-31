8b630776922b64818fca2b3393df70893f44d3ed
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-05-31 22:14:16 +0200

    update a program that imports functions from the file calculator_1.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-from calculator_1 import add, subtract, multiply, divide
-a = 10
-b = 5
+from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    a = 10
+    b = 5
 
-somme = add(a, b)
-difference = subtract(a, b)
-produit = multiply(a, b)
-quotient = divide(a, b)
-
-print("Résultats des opérations :")
-print("Somme de {} et {} = {}".format(a, b, somme))
-print("Différence entre {} et {} = {}".format(a, b, difference))
-print("Produit de {} par {} = {}".format(a, b, produit))
-print("Quotient de {} par {} = {}".format(a, b, quotient))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
