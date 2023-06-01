0ea21f61cb20eb358564cff091b219c3598c5da1
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-06-01 14:08:18 +0200

    update a program that prints all the names defined by the compiled module

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-import hidden_4
 if __name__ == "__main__":
-    
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    import hidden_4
+    for name in dir(hidden_4):
+        if name[0] != '_' and name[1] != '_':
+            print("{}".format(name))
