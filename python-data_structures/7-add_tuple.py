061f601686e350c37fa1f9823a4b65877b9c6847
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-06-07 20:02:38 +0200

    update a function that adds 2 tuples.

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+
+    tuple_result = ()
     tuple_a = tuple_a[:2]
     tuple_b = tuple_b[:2]
-
-    result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    return result
+    tuple_result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return tuple_result
