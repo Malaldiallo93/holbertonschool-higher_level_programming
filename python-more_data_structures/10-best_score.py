2b83b943f3fc386e4d040d4ef82ba539288dd3f9
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-06-08 21:36:22 +0200

    update a function that returns a key with the biggest integer value.

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     max_key = None
-    max_value = float('-inf')
+    max_value = 0
 
     for key, value in a_dictionary.items():
         if value > max_value:
