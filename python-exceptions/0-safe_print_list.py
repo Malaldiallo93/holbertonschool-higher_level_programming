98ab31095436938f084aa6399c3c115e31fb1b23
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-06-09 21:23:16 +0200

    update a function that prints x elements of a list.

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for element in my_list[:x]:
-            print(element, end=" ")
-        print()
-        return min(x, len(my_list))
-    except:
-        return 0
+    count = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            count += 1
+        except IndexError:
+            pass
+    print()
+    return count
