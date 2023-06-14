6a964c62025fa47fc294b7bb48ef8c425e13bf12
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-06-14 12:43:40 +0200

    update a class Square that defines a square on 3-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -19,6 +19,7 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+
     """Returns the current square area"""
     def area(self):
         return self.__size * self.__size
