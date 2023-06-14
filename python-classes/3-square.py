6069d145cebf7db004b060454775535b013e3a1b
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-06-14 12:32:59 +0200

    update a class Square that defines a square on 2-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -3,6 +3,7 @@
 
 
 class Square:
+    """Define your square by size (private instance attribute)"""
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
@@ -10,5 +11,7 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
+
+    """Returns the current square area"""
     def area(self):
         return self.__size * self.__size
