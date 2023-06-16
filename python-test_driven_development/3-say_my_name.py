42ca5eedc23c014b0d9fcda334d2cf8c7caee360
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-06-16 20:36:54 +0200

    update a function say my name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -10,7 +10,9 @@ def say_my_name(first_name, last_name=""):
     """Function that prints My name is
     <first name> <last name>
     """
-    if not isinstance(first_name, str) or not isinstance(last_name, str):
+    if not isinstance(first_name, str):
+         raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     full_name = first_name + " " + last_name
     print("My name is", full_name)
