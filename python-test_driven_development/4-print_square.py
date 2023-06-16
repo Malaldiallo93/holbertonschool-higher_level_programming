e7dbbc75360168faa46dbe2cb6b4d144b414dd53
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-06-16 20:59:01 +0200

    update say my name function

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -6,6 +6,9 @@
 
 
 def print_square(size):
+    """Function that prints a square
+    with the character #
+    """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
