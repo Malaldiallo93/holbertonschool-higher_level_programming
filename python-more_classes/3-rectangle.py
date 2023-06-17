64012a88fcef58b668552b9a9353c53cc9b3edc3
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-06-17 16:13:19 +0200

    upda

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -10,10 +10,8 @@ Args:
 
 
 class Rectangle:
-    """Define your rectangle with width and height"""
+    """Defines a rectangle with width and height"""
     def __init__(self, width=0, height=0):
-        self.__width = 0
-        self.__height = 0
         self.width = width
         self.height = height
 
@@ -23,11 +21,12 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
+        else:
+            self.__width = value
 
     @property
     def height(self):
@@ -35,24 +34,31 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
+        else:
+            self.__height = value
 
     def area(self):
+        """Returns the rectangle area"""
         return self.__width * self.__height
 
     def perimeter(self):
+        """Returns the rectangle perimeter"""
         if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * (self.__width + self.__height)
+        return (self.__width + self.__height) * 2
 
     def __str__(self):
+        """Print the rectangle with the character #"""
+        str = ""
         if self.__width == 0 or self.__height == 0:
-            return ""
-        rectangle_str = ""
-        for _ in range(self.__height):
-            rectangle_str += "#" * self.__width + "\n"
-        return rectangle_str
+            return str
+        for column in range(self.__height):
+            for row in range(self.__width):
+                str += "#"
+            if column is not self.__height - 1:
+                str += "\n"
+        return str
