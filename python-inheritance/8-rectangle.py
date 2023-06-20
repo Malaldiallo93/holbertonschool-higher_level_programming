3d3e3f8cb63a15eb8c20172e7447bffb2deddfe4
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-06-20 21:48:01 +0200

    update  a class Rectangle that inherits from BaseGeometry (7-base_geometry.py).

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -3,7 +3,12 @@
 
 
 class Rectangle(BaseGeometry):
-    """Class Rectangle, subclass of BaseGeometry"""
+    """Class Rectangle, subclass of BaseGeometry
+
+    comments:
+            width (int): width of the rectangle
+            height (int): height of the rectangle
+    """
     def __init__(self, width, height):
         """Instantiation using BaseGeometry(superclass) function"""
         super().integer_validator("width", width)
