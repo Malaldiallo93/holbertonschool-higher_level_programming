ea812bd27cbc7038384f02594e2ca61175a63c14
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-06-20 21:04:33 +0200

    update a function that returns True if the object is an instance of otherwise False.

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -6,7 +6,5 @@ def is_kind_of_class(obj, a_class):
     """function that returns True, otherwise False. """
     if isinstance(obj, a_class):
         return True
-    for cls in obj.__class__.__bases__:
-        if is_kind_of_class(cls, a_class):
-            return True
-    return False
+    else:
+        return False
