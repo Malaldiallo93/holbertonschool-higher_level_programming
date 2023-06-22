c49108692d5a13b1554553e963f1e000025029d6
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-06-22 13:48:41 +0200

    update a function that reads a text file (UTF8) and prints it to stdout

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Read file"""
+
+
 def read_file(filename=""):
+    """Methode that reads a text file (UTF8) and prints it to stdout"""
     with open(filename, "r", encoding="utf-8") as file:
         contents = file.read()
         print(contents)
