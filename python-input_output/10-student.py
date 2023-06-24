d042b183a579d19335738a243043b92c66a3fe98
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-06-24 13:58:15 +0200

    add a class Student that defines a student by: (based on 9-student.py)

---
+++ python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Student to JSON with filter"""
+
+
+class Student:
+    """Class Student that defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """Instantiation of public attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Function that retrieves a dictionary representation
+        of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_dict = {key: self.__dict__[key] for key in self.__dict__
+                        if key in attrs}
+            return new_dict
